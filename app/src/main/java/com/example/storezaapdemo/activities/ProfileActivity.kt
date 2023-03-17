9cee3ce938f42c58ebe30a91c3539e73e7b9f2bd
venkatnaresh <venkatnaresh234@gmail.com>
2023-03-17 16:15:11 +0530

    This is my task

--- app/src/main/java/com/example/storezaapdemo/activities/ProfileActivity.kt
+++ app/src/main/java/com/example/storezaapdemo/activities/ProfileActivity.kt
@@ -32,7 +32,7 @@ class ProfileActivity : AppCompatActivity() {
 
         sharedPrefManager = SharedPrefManager(this)
 
-        val userName = "Hey! " + sharedPrefManager.getUser().username
+        val userName = "Hey! " + sharedPrefManager.getUser()
         etname.text = userName
         etemail.text = sharedPrefManager.getUser().email
 
